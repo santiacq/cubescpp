59d5d97a349b22ee7dd1f63e9b40c3f07d8f1547
Santiago Acquarone <santiacquarone@gmail.com>
2022-10-23 16:12:06 -0300

    erase debug comments

--- src/render.cpp
+++ src/render.cpp
@@ -67,8 +67,5 @@ void Render::render(Player player, Chunk &chunk) {
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, chunk.getMesh()->getTriangles() * 9 * sizeof(float), chunk.getMesh()->getVertices(), GL_DYNAMIC_DRAW);
 
-    //shader.use();
-    //glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
     glDrawArrays(GL_TRIANGLES, 0, chunk.getMesh()->getTriangles() * 3);
-    //std::cout << "here 7" << std::endl;
 }
